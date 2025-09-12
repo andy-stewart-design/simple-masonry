c87bb391d9c6fe16a0fe9e489fa2bd2f787cceb4
andy-stewart-design <robestewart@ebay.com>
2025-09-12 12:41:37 -0400

    Getting initial implementation working

--- app/components/masonry-grid/index.tsx
+++ app/components/masonry-grid/index.tsx
@@ -1,29 +1,106 @@
-import { useRef, type ComponentProps, type CSSProperties } from "react";
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  Children,
+  type ComponentProps,
+  type CSSProperties,
+} from "react";
 import { getSrcFromNodes } from "~/utils/get-src-from-node";
 import "./style.css";
 
 function MasonryGrid({ children }: ComponentProps<"div">) {
-  if (!Array.isArray(children)) return null;
-
-  const groupSize = useRef(children.length);
+  const gridRef = useRef<HTMLDivElement>(null);
+  const groupSize = useRef(Children.toArray(children).length);
   const items = getSrcFromNodes(children);
 
+  const resizeGridItem = useCallback((item: HTMLElement) => {
+    const grid = gridRef.current;
+    if (!grid) return;
+
+    const rowHeight = parseInt(
+      window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
+    );
+    const rowSpan = Math.ceil(
+      item.firstElementChild!.getBoundingClientRect().height / rowHeight
+    );
+    item.style.gridRowEnd = "span " + rowSpan;
+  }, []);
+
+  useEffect(() => {
+    const grid = gridRef.current;
+    if (!grid) return;
+
+    function resizeAllGridItems() {
+      document.querySelectorAll<HTMLElement>(".grid-item").forEach((item) => {
+        resizeGridItem(item);
+      });
+    }
+
+    const observer = new ResizeObserver(() => resizeAllGridItems());
+    observer.observe(grid);
+    resizeAllGridItems();
+
+    return () => {
+      observer.unobserve(grid);
+      observer.disconnect;
+    };
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    document.querySelectorAll<HTMLElement>(".grid-item").forEach((item) => {
+      item
+        .querySelector<HTMLImageElement>("img")
+        ?.addEventListener("load", () => resizeGridItem(item), {
+          signal,
+        });
+    });
+
+    return () => controller.abort();
+  }, [children]);
+
+  if (!Array.isArray(children)) return null;
+
   return (
-    <div className="masonry-grid">
+    <div ref={gridRef} className="masonry-grid">
       {Array.from(new Map(items)).map(([id, node], i) => (
-        <div
+        <MasonryGridItem
           key={id}
-          className="grid-item"
-          style={
-            {
-              "--grid-index": i,
-              "--group-index": i % groupSize.current,
-            } as CSSProperties
-          }
+          gridIndex={i}
+          groupIndex={i % groupSize.current}
         >
           {node}
-        </div>
+        </MasonryGridItem>
       ))}
+    </div>
+  );
+}
+
+function MasonryGridItem({
+  children,
+  gridIndex,
+  groupIndex,
+}: ComponentProps<"div"> & { gridIndex: number; groupIndex: number }) {
+  const [rowSpan, setRowSpan] = useState<number | undefined>(undefined);
+  const itemRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div
+      ref={itemRef}
+      className="grid-item"
+      style={
+        {
+          "--grid-index": gridIndex,
+          "--group-index": groupIndex,
+          "--row-span": rowSpan,
+        } as CSSProperties
+      }
+    >
+      <div>{children}</div>
     </div>
   );
 }
